a046f81e5a50660b4f99ce487aab84dcbdfb28c9
Anil_Ramcharran <anil.gitme@gmail.com>
2021-04-01 23:12:07 -0400

    complete!

--- api/schemes/scheme-middleware.js
+++ api/schemes/scheme-middleware.js
@@ -1,3 +1,4 @@
+const db = require('../../data/db-config');
 /*
   If `scheme_id` does not exist in the database:
 
@@ -6,9 +7,23 @@
     "message": "scheme with scheme_id <actual id> not found"
   }
 */
-const checkSchemeId = (req, res, next) => {
-
+const checkSchemeId = async(req, res, next) => {
+    try {
+        const { scheme_id } = await req.params
+        const scheme = await db('schemes').where(scheme_id).first()
+        if (!scheme) {
+            //if no scheme found
+            return res.status(404).json({ message: `scheme with ${scheme_id} not found` })
+        } else {
+            res.status(200).json(scheme)
+            next()
+        }
+    } catch (err) {
+        next(err)
+    }
 }
+
+
 
 /*
   If `scheme_name` is missing, empty string or not a string:
@@ -18,8 +33,17 @@ const checkSchemeId = (req, res, next) => {
     "message": "invalid scheme_name"
   }
 */
-const validateScheme = (req, res, next) => {
-
+const validateScheme = async(req, res, next) => {
+    try {
+        const { scheme_name } = await req.body;
+        if (scheme_name === '' || !scheme_name || typeof scheme_name !== 'string') {
+            res.status(400).json({ message: 'invalid scheme_name' })
+        } else {
+            next()
+        }
+    } catch (err) {
+        next(err)
+    }
 }
 
 /*
@@ -31,12 +55,22 @@ const validateScheme = (req, res, next) => {
     "message": "invalid step"
   }
 */
-const validateStep = (req, res, next) => {
-
+const validateStep = async(req, res, next) => {
+    try {
+        const { instructions, step_number } = req.body;
+        if (!instructions || instructions === '' || typeof instructions !== 'string' ||
+            step_number !== 'number' || step_number < 1) {
+            res.status(400).json({ message: 'invalid step' })
+        } else {
+            next()
+        }
+    } catch (err) {
+        next(err)
+    }
 }
 
 module.exports = {
-  checkSchemeId,
-  validateScheme,
-  validateStep,
+    checkSchemeId,
+    validateScheme,
+    validateStep,
 }
